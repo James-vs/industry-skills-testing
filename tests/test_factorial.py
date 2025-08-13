332c200228843ad3f49cbdb737b7c991218c712a
jcvs1g20 <jcvs1g20@soton.ac.uk>
2025-08-13 14:21:12 +0100

    Add tests for 4, 5, 6, and -1

--- tests/test_factorial.py
+++ tests/test_factorial.py
@@ -2,5 +2,22 @@ import pytest
 from mymath.factorial import factorial
 
 
+def test_1():
+    assert factorial(4) == 24
+
+
+def test_2():
+    assert factorial(5) == 120
+
+
 def test_3():
     assert factorial(3) == 6
+
+
+def test_4():
+    assert factorial(6) == 720
+
+
+def test_5_negative():
+    with pytest.raises(ValueError):
+        factorial(-1)
